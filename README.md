169474bec144c3a6ea4a48825ec67aefade6c8f2
ROllerozxa <temporaryemail4meh+github@gmail.com>
2020-03-07 19:44:01 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Featured List Creator
+This is a Python program that generates a featured list file from JSON data and images inside of `data/`.
+
+This file can then be put into your Principia user folder (as fl.cache) or onto a replacement web server to be downloaded by a Principia client.
---
+++ main.py
@@ -0,0 +1,37 @@
+import json
+import struct
+from pathlib import Path
+
+def u32(data):
+	if not 0 <= data <= 4294967295:
+		print("u32 out of range: %s" % data, "INFO")
+		data = 0
+	return struct.pack("<I", data)
+
+def main():
+	data = json.loads(open(Path("data/data.json"), "r").read())
+
+	with open('fl.cache', 'wb+') as f:
+		f.write(u32(len(data['featured_levels'])))		# featured_level_count
+		for feat_level in data['featured_levels']:
+			with open(Path(feat_level['jpeg_image']), "rb") as jpeg_file:
+				jpeg_stream = jpeg_file.read()
+
+			f.write(u32(feat_level['id']))				# fl_id
+			f.write(u32(len(feat_level['name'])))		# fl_name_size
+			f.write(feat_level['name'].encode())		# fl_name
+			f.write(u32(len(feat_level['author'])))		# fl_author_size
+			f.write(feat_level['author'].encode())		# fl_author
+			f.write(u32(len(jpeg_stream)))				# fl_jpegstream_size
+			f.write(jpeg_stream)						# fl_jpegstream
+
+		f.write(u32(0))									# unknown_7ef28c
+		f.write(u32(len(data['gettingstarted_list'])))	# gettingstarted_list_count
+		for gettingstarted in data['gettingstarted_list']:
+			f.write(u32(len(gettingstarted['name'])))	# gs_name_size
+			f.write(gettingstarted['name'].encode())	# gs_name
+			f.write(u32(len(gettingstarted['link'])))	# gs_link_size
+			f.write(gettingstarted['link'].encode())	# gs_link
+
+if __name__ == "__main__":
+	main()
